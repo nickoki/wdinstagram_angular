9456ddc08acccd7a236c0f33d4b01b644ae622b5
Nick Oki <nick.oki@live.com>
2016-10-25 17:48:21 -0400

    Angular dependencies, factory Index route and controller

--- js/app.js
+++ js/app.js
@@ -1,5 +1,51 @@
 "use strict";
 
-(function(){
-  angular.module("wdinstagram", [])
-})();
+// Note: function wrapped in an IFEE (Immediately Invoked Function Expression)
+(function() {
+
+  angular
+    .module("wdinstagram", [
+      "ui.router",
+      "ngResource"
+    ])
+    .config([
+      "$stateProvider",
+      RouterFunction
+    ])
+    .factory("InstaFactory", [
+      "$resource",
+      InstaFactoryFunction
+    ])
+    .controller("InstaIndexController", [
+      "InstaFactory",
+      InstaIndexControllerFunction
+    ]);
+
+
+
+  // Factory Function
+  function InstaFactoryFunction($resource) {
+    return $resource("http://localhost:3000/entries/:id", {}, {
+      update: { method: "PUT" }
+    })
+  }
+
+  // Controller Functions
+  function InstaIndexControllerFunction(InstaFactory) {
+    this.posts = InstaFactory.query()
+  }
+
+
+
+  // Router
+  function RouterFunction($stateProvider) {
+    $stateProvider
+    .state("instaIndex", {
+      url: "/",
+      templateUrl: "js/ng-views/index.html",
+      controller: "InstaIndexController",
+      controllerAs: "vm"
+    })
+  };
+
+})(); // close IFEE
